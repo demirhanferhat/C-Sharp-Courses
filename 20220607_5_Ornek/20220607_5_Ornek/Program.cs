1241113c06c872bff5d145e96393a2c52735f1c7
Ferhat Demirhan <ferhat.fettahoglu@gmail.com>
2022-08-23 21:24:45 +0300

    IstheNumberEnteredOdd?Couple?
    Is the Number Entered Odd ? Couple?

---
+++ 20220607_5_Ornek/20220607_5_Ornek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220607_5_Ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan 3 adet sayı talep edelim. En büyük ve En küçük sayıyı ekrana yazdıralım.
+        }
+    }
+}
---
+++ 20220610_1_Ornek/20220610_1_Ornek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220610_1_Ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan 1 adet sayı talep edelim.
+            // Girilen sayı tek mi? çift mi? ekrana yazdıralım.
+
+            Console.Write("Bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayı çifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Sayı tektir.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
