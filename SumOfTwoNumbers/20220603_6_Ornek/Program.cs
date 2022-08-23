4573e8a10b7c00d1e4225d5bf1557b45650d0a8d
Ferhat Demirhan <ferhat.fettahoglu@gmail.com>
2022-08-23 20:27:54 +0300

    SumOfTwoNumbers
    Sum Of Two Numbers

---
+++ SumOfTwoNumbers/20220603_6_Ornek/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan 2 adet sayı talep edelim. Sayıların toplamını ekrana yazdıralım.
+
+            Console.Write("1. sayıyı giriniz: ");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+
+            Console.Write("2. sayıyı giriniz: ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sayıların Toplamı: " + (sayi1 + sayi2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
